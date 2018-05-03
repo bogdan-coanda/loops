b7f9440627f0cbbe0cba38368239552b21bb47a7
Kaly <bogdan-coanda@users.noreply.github.com>
2018-05-03 23:23:20 +0300

    loading roads

--- py/explorer.py
+++ py/explorer.py
@@ -11,6 +11,12 @@ if __name__ == "__main__":
 	#superperms = [d.generateDiagramForKnownID(ik).superperm() for ik in range(len(d.knowns))]
 	#print("Found " + str(len(set(superperms))) + " distinct superperms")
 	
-	roads = [diagram.generateDiagramForKnownID(ik).road() for ik in range(len(diagram.knowns))]
-	print("Found " + str(len(set(roads))) + " distinct roads")
+	#roads = [diagram.generateDiagramForKnownID(ik).road() for ik in range(12)]#len(diagram.knowns))]
+	#print("Found " + str(len(set(roads))) + " distinct roads")
 	
+	#with open("roads."+str(diagram.spClass)+".pkl", 'wb') as outfile:
+		#pickle.dump(set(roads), outfile, 0)
+
+	with open('roads.'+str(diagram.spClass)+".pkl", 'rb') as infile:	
+		roads = list(pickle.load(infile))
+	print("Loaded "+str(len(roads))+" unique roads")
