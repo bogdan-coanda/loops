98b60c466b647a0592a203025c726eb53b180703
kaly <bogdan.coanda@gmail.com>
2019-02-25 08:37:35 +0200

    …

--- w0/6b.py
+++ w0/6b.py
@@ -4,20 +4,27 @@ from uicanvas import *
 
 if __name__ == "__main__":
 		
-	diagram = Diagram(6, 1)
+	diagram = Diagram(6, 0)
 	
 	import enav
 	enav.diagram = diagram
 	from enav import *
 	
 	# ------------------------------ #
-	
-	extend('00001')
-	
-	el('10005', 3)
-	es('12305', 2)
-	el('12205', 5)
-	es('12105', 4)
+
+	el('00005', diagram.bases[0].ktype)
+	el('00105', diagram.bases[1].ktype)
+	el('00205', diagram.bases[2].ktype)
+	el('00305', diagram.bases[3].ktype)
+			
+	el('00105', 4)
+			
+	# extend('00001')
+	# 
+	# el('10005', 3)
+	# es('12305', 2)
+	# el('12205', 5)
+	# es('12105', 4)
 	
 	#et('10105')
 	#et('11005')
--- w0/8e.py
+++ w0/8e.py
@@ -18,10 +18,22 @@ if __name__ == "__main__":
 
 	# ---------------------------- #
 		
-	nx += elt('0000007', 6)
-	
+	nx += el('0000007', diagram.bases[0].links[1].next.links[1].next.links[1].next.links[1].next.links[1].next.links[1].next.links[1].next.ktype)
+	nx += el('0000107', diagram.bases[1].prevs[1].node.prevs[1].node.prevs[1].node.prevs[1].node.prevs[1].node.prevs[1].node.prevs[1].node.ktype)
+	nx += el('0000207', diagram.bases[2].links[1].next.links[1].next.links[1].next.links[1].next.links[1].next.links[1].next.links[1].next.ktype)
+	nx += el('0000307', diagram.bases[3].prevs[1].node.prevs[1].node.prevs[1].node.prevs[1].node.prevs[1].node.prevs[1].node.prevs[1].node.ktype)
+	nx += el('0000407', diagram.bases[4].links[1].next.links[1].next.links[1].next.links[1].next.links[1].next.links[1].next.links[1].next.ktype)
+	nx += el('0000507', diagram.bases[5].prevs[1].node.prevs[1].node.prevs[1].node.prevs[1].node.prevs[1].node.prevs[1].node.prevs[1].node.ktype)
+		
+	nx = elt('1000007', 3)
+	nx = elt('0100007', 2)
+	# nx = elt('0011007', 5)
+		
+	# nx += el('1000007', 6)
+	# nx += el('0001507', 2)	
+	# nx += el('0210207', 4)
 	
 	# diagram.point()
-	diagram.pointers = list(itertools.chain(*[n.loop.nodes for n in nx]))	
+	diagram.pointers = list(itertools.chain(*[n.loop.nodes for n in nx]))
 	show(diagram)
 
